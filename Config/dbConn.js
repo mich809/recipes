61e1f3bd2c5d3370402a02479d4f8155c17f418d
Michael Caridad <caridadmichael@gmail.com>
2023-01-31 00:35:50 -0400

    establish db connection

--- Config/dbConn.js
+++ Config/dbConn.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const connectDB = async () =>{
     try{
         await mongoose.connect(process.env.DATABASE_URI)
+        console.log('Connected to MongoDB')
     }catch(err){
         console.log(err)
 
---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3500
+
+
+connectDB()
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+})
